16cd0a820a37cebe4e8ebd0a0ac01744da551bd9
Vladislav Koltovich <vladislav.a.koltovicha@gmail.com>
2025-08-20 05:41:11 +0300

    add: netlify.toml

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { AuthProvider } from './contexts/AuthContext'
 import RegisterPage from './Pages/RegisterPage'
 import LoginPage from './Pages/LoginPage'
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage/>} />
-          <Route path="/register" element={<RegisterPage/>} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider> 
+    <Routes>
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/register" element={<RegisterPage/>} />
+    </Routes> 
   )
 }
 
